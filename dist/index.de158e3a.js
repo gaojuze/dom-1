05748bdf3662c1bd80303941153a470e55595b25
gaojuze <juze.gao@qq.com>
2022-07-12 00:41:02 +0800

    update

---
+++ dist/index.de158e3a.js
@@ -0,0 +1,8 @@
+const div = dom.find("#test>.red")[0];
+console.log(div);
+dom.style(div, "color", "green");
+const divList = dom.find(".red");
+dom.each(divList, (n)=>console.log(n)
+);
+
+//# sourceMappingURL=index.de158e3a.js.map
